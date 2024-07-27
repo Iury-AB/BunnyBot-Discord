4bd7b3fa8ba0b75035bb7be3208f08fbb81047bb
Iury-AB <iuryalves.iab@gmail.com>
2024-07-27 16:44:13 -0300

    keep-alive

--- src/index.js
+++ src/index.js
@@ -457,8 +457,7 @@ client.on('interactionCreate', async (interaction) => {
   if(interaction.commandName === 'bind') {
     // Check if the user has permission to manage channels
     if (!interaction.member.permissions.has('MANAGE_CHANNELS')) {
-      await interaction.reply({ content: 'You do not have permission to bind the bot to a channel.', ephemeral: true });
-      return;
+      return interaction.reply({ content: 'You do not have permission to bind the bot to a channel.', ephemeral: true });
     }
     const channelOption = interaction.options.getChannel('channel');
     
