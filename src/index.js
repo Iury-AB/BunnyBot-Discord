072e583e8fae2a2d60078c3660880336484c7c39
Iury-AB <iuryalves.iab@gmail.com>
2026-04-19 14:09:47 -0300

    prioriza ipv4

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 require('dotenv').config();
+
+const dns = require('node:dns');
+dns.setDefaultResultOrder('ipv4first');
+
 const express = require('express');
 const fs = require('fs');
 // const keep_alive = require('./keep_alive.js');
